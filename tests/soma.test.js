3e264c42b2a18484a4da90095eb6a628ca693806
Kethler Almeida <almeidasantoskethler@gmail.com>
2023-01-05 00:25:43 -0300

    primeiro comentario

--- tests/soma.test.js
+++ tests/soma.test.js
@@ -1,7 +1,7 @@
 const soma = require('./../src/soma')
 
-test('Adicione 1 + 2 e resulte em 3', () => {
+test('Adicione 5 + 7 e resulte em 12', () => {
 
-    expect(soma(1, 2)).toBe(3);
+    expect(soma(5, 7)).toBe(12);
 
 });
